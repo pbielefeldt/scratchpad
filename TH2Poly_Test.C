19bb1d1f28524d2b19f4d894fdc148dc451b4f7c
Philipp Bielefeldt <bielefeldt@uni-bonn.de>
2019-10-01 16:21:05 +0200

    Add TH2Poly test script
    Wanted to check if TH2Poly::Honeycomb has an issue with clear-ing …

---
+++ TH2Poly_Test.C
@@ -0,0 +1,39 @@
+// A test script to investigate a potential memory leak in TH2Poly::Honeycomb
+//
+// (C) 2019, P. Bielefeldt, Universität Bonn
+// Available under Mozilla Public License 2.0+
+//
+// Use (in a ROOT shell)
+//   .L TH2Poly_Test.C+
+//   TH2Poly_Test()
+
+#include <TROOT.h>
+#include <TRandom2.h>
+#include <TH2Poly.h>
+
+void TH2Poly_Test()
+{
+  const UInt_t large_number = 5000; 
+  
+  // create Honeycomb structure once
+  TH2Poly* my_histo = new TH2Poly;
+  my_histo->Honeycomb(-15., -15., 0.15, 115, 134);
+
+  TRandom* rng = new TRandom2();
+  
+  for (UInt_t i=0; i<large_number; i++)
+  {
+    my_histo->Clear();
+    
+    // just some filling
+    for (UInt_t j=0; j<large_number; j++)
+    {
+      Double_t some_x = rng->Uniform(30)-15;
+      Double_t some_y = rng->Uniform(30)-15;
+      Double_t some_v = rng->Uniform(42);
+      my_histo->Fill(some_x, some_y, some_v);
+    }
+    
+  }
+  
+}
