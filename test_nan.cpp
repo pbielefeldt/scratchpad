5c94f7a6d4929620b286b2f851c127a3ee118363
Philipp Bielefeldt <bielefeldt@uni-bonn.de>
2020-05-29 18:08:51 +0200

    more explicit

--- test_nan.cpp
+++ test_nan.cpp
@@ -10,10 +10,12 @@ int main()
 	const double x1 = 4.2415926; // not a nan
 	const double x2 = sqrt(-1.); // a nan
 	
-	const bool nan1 = isnan(x1);
-	const bool nan2 = isnan(x2);
 	const bool any_nan = isnan(x1) || isnan(x2);
 	const bool all_nan = isnan(x1) && isnan(x2);
 	
-	std::cout << "1. " << nan1 << ", 2. " << nan2 << "\nany: " << any_nan << ", all: " << all_nan << std::endl; 
+	if (any_nan)
+		std::cout << "one of the numbers was a NaN" << std::endl; 
+
+	if (all_nan)
+		std::cout << "all of the numbers were NaN" << std::endl; 
 }
