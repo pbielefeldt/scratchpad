0938a5102569d5d12cc33a4e37834e1379d9cc0f
Philipp Bielefeldt <p.bielefeldt@secure.mailbox.org>
2021-03-01 14:30:10 +0100

    make v a pointer, too

--- pass_pointer.cpp
+++ pass_pointer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-/* Test pushing to a vector
+/* Test transferring pointers from vectors to functions
  * 
 g++ -Wall -o pass_pointer.o pass_pointer.cpp 
  */
@@ -39,19 +39,19 @@ public:
  
  int main()
  {
- 	vector<my_class*> v;
+ 	vector<my_class*>* v = new vector<my_class*>;
  	
  	// create the objects
  	for (int i=0; i<14; i++)
  	{
  		my_class* c = new my_class();
- 		v.push_back(c);
+ 		v->push_back(c);
  	}
  	
  	// modify the objects
- 	for (size_t i=0; i<v.size(); i++)
+ 	for (size_t i=0; i<v->size(); i++)
  	{
- 		my_class* c_ptr = dynamic_cast<my_class*>(v.at(i));
+ 		my_class* c_ptr = dynamic_cast<my_class*>(v->at(i));
  		cout << c_ptr->get_val() << endl;
  		
  		if (i%3 == 0)
@@ -63,7 +63,7 @@ public:
  	cout << "\n----\n" << endl;
  	
  	// print
- 	for (auto& i : v)
+ 	for (auto& i : *v)
  	{
  		cout << i->get_val() << endl;
  	}
